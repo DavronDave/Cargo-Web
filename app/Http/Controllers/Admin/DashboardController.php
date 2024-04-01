5c9c3bd7da2c6893920195e79ff9ff0ba1f891f4
davronomonov99 <davronomonov99@gmail.com>
2024-04-01 09:53:20 +0500

    Dashboard index bugs fixed

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -12,56 +12,27 @@ class DashboardController extends Controller
 {
     public function index()
     {
-//        $admin = DataLog::join('users', 'data_logs.user_id', '=', 'users.id')
-////            ->where('users.id', '=', '1')
-//            ->where('data_logs.created_at', '>=', now()->startOfMonth()) // Filter for the current month
-//            ->select(
-//                'users.full_name as full_name',
-//                DB::raw('TO_CHAR(data_logs.created_at, \'Month\') as month_name'),
-//                DB::raw('SUM(CASE WHEN data_type = \'invoices\' THEN 1 ELSE 0 END) as total_invoices'),
-//                DB::raw('SUM(CASE WHEN data_type = \'passport\' THEN 1 ELSE 0 END) as total_passports')
-//            )
-//            ->groupBy('users.full_name', 'month_name')
-//            ->get();
-//        dd($admin);
-
-//        $moderator = DataLog::join('users', 'data_logs.user_id', '=', 'users.id')
-//            ->where('users.id', '=', '3')
-//            ->where('data_logs.created_at', '>=', now()->startOfMonth()) // Filter for the current month
-//            ->select(
-//                'users.full_name as full_name',
-//                DB::raw('TO_CHAR(data_logs.created_at, \'Month\') as month_name'),
-//                DB::raw('SUM(CASE WHEN data_type = \'invoices\' THEN 1 ELSE 0 END) as total_invoices'),
-//                DB::raw('SUM(CASE WHEN data_type = \'passport\' THEN 1 ELSE 0 END) as total_passports')
-//            )
-//            ->groupBy('users.full_name', 'month_name')
-//            ->get();
-
         $thisMonth = DataLog::join('users', 'data_logs.user_id', '=', 'users.id')
 //            ->where('users.id', '=', '3')
             ->whereMonth('data_logs.created_at', '=', now()->month)
+//            ->select(
+//                'users.full_name as full_name',
+////                DB::raw('TO_CHAR(data_logs.created_at, \'Month\') as month_name'),
+//                DB::raw('DATE_FORMAT(data_logs.created_at, \'%M\') as month_name'),
+//                DB::raw('SUM(CASE WHEN data_type = \'invoices\' THEN 1 ELSE 0 END) as total_invoices'),
+//                DB::raw('SUM(CASE WHEN data_type = \'passport\' THEN 1 ELSE 0 END) as total_passports')
+//            )
             ->select(
                 'users.full_name as full_name',
-//                DB::raw('TO_CHAR(data_logs.created_at, \'Month\') as month_name'),
-                DB::raw('DATE_FORMAT(data_logs.created_at, \'%M\') as month_name'),
+                DB::raw('DATE_FORMAT(data_logs.created_at, \'%M\') as month_name'), // Use '%M' to get the full month name
+                DB::raw('EXTRACT(YEAR FROM data_logs.created_at) as year'), // Extract year
                 DB::raw('SUM(CASE WHEN data_type = \'invoices\' THEN 1 ELSE 0 END) as total_invoices'),
-                DB::raw('SUM(CASE WHEN data_type = \'passport\' THEN 1 ELSE 0 END) as total_passports')
+                DB::raw('SUM(CASE WHEN data_type = \'passports\' THEN 1 ELSE 0 END) as total_passports')
             )
+
             ->groupBy('users.full_name', 'month_name')
             ->get();
 
-//        $exceptMonth = DataLog::join('users', 'data_logs.user_id', '=', 'users.id')
-////            ->where('users.id', '=', '3')
-//            ->whereMonth('data_logs.created_at', '!=', now()->month) // Exclude the current month
-//            ->select(
-//                'users.full_name as full_name',
-//                DB::raw('TO_CHAR(data_logs.created_at, \'Month\') as month_name'),
-//                DB::raw('SUM(CASE WHEN data_type = \'invoices\' THEN 1 ELSE 0 END) as total_invoices'),
-//                DB::raw('SUM(CASE WHEN data_type = \'passports\' THEN 1 ELSE 0 END) as total_passports')
-//            )
-//            ->groupBy('users.full_name', 'month_name')
-//            ->orderBy('month_name')
-//            ->get();
 
         $exceptMonth = DataLog::join('users', 'data_logs.user_id', '=', 'users.id')
             //->where('users.id', '=', '3')
