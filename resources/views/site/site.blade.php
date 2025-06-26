fe68cfc2267e3b5254c2c6f4130016cb31bb325c
Davron_jr <jahongire085@gmail.com>
2025-06-26 17:39:49 +0500

    front changed

--- resources/views/site/site.blade.php
+++ resources/views/site/site.blade.php
@@ -523,7 +523,7 @@
             }
 
             .stat-number {
-                font-size: 2.0rem;
+                font-size: 2.2rem;
             }
 
             .stat-label {
@@ -531,7 +531,7 @@
             }
 
             .contact-item {
-                font-size: 0.7rem;
+                font-size: 0.8rem;
                 text-decoration: none;
                 text-underline-position: unset
             }
